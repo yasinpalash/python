641cac9b78f4230c2e80c8e59e91d6d0ebfc24fe
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-03-09 15:00:50 +0600

    add variables.py and vscode settings for Python environment

---
+++ Variables/variables.py
@@ -0,0 +1,33 @@
+"""
+Definition:
+A variable is a container used to store data values in memory.
+
+Python is a dynamically typed language, which means:
+- You do NOT need to declare the data type explicitly.
+- Python automatically determines the data type.
+
+Example:
+x = 10
+
+Here:
+x = variable name
+10 = value stored inside the variable
+"""
+# ------------------------------------------------------
+# 1. Creating Variables
+# ------------------------------------------------------
+
+# A variable can store numbers
+age=25
+
+# A variable can store text (string)
+name="Yasin"
+# A variable can store decimal numbers (float)
+height=7.56
+
+
+# Printing variables
+print(age)
+print(name)
+print(height)
+
